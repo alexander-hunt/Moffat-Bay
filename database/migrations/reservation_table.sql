b3e1d5a63db491c48b375e6e9b465b12634fcc29
jumorrow-school <jumorrow-school@users.noreply.github.com>
2026-08-27 21:55:34 -0600

    Updated filed to include a drop to prevent duplications and have only 1 record being added

--- database/migrations/reservation_table.sql
+++ database/migrations/reservation_table.sql
@@ -6,7 +6,10 @@
 	Note: Noor's and Carli's files can be ran first or second. This file has to be ran last due to foreign keys.
 */
 
--- Create the Reservation table
+-- Drop table if it exists to prevent duplications
+DROP TABLE IF EXISTS Reservation;
+
+-- Create the Reservation Table
 CREATE TABLE Reservation (
     ReservationID INT(10) NOT NULL AUTO_INCREMENT,
     UserID INT(10) NOT NULL,
@@ -24,11 +27,8 @@ CREATE TABLE Reservation (
         REFERENCES Room(RoomID)
 );
 
--- Insert sample Reservation records
+-- Insert Reservation Record
 INSERT INTO Reservation
     (UserID, RoomID, Booking_Date, Date_In, Date_Out, Guest_Count)
 VALUES
-    (1, 1, '2026-08-27', '2026-09-10', '2026-09-15', 2),
-    (2, 2, '2026-08-27', '2026-08-28', '2026-08-31', 4),
-	(3, 3, '2026-08-27', '2026-09-12', '2026-09-16', 5),
-    (4, 4, '2026-08-27', '2026-10-01', '2026-10-05', 8);
+    (1, 2, '2026-08-27', '2026-08-28', '2026-08-31', 4);
